5ba33843f9a8a81fcafc1d08e20eec8d22486ed8
Donte Wilson <JittNA@aol.com>
2016-10-12 13:10:50 -0500

    10/12/16
    Worked on seek and fixed agent

---
+++ Assets/Agent.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class Agent : MonoBehaviour
+{
+    public Vector3 force;
+    public Vector3 desiredv;
+    public Vector3 velocity;
+    public int mass;
+    public Vector3 steering;
+    public Transform target;
+
+
+	// Use this for initialization
+	void FixedUpdate () {
+        steering = (desiredv - velocity).normalized;
+        desiredv = (target.position - transform.position).normalized / mass;
+        velocity += steering;
+        if (velocity.magnitude > 5)
+            velocity = velocity.normalized;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position += velocity;
+        
+        
+	
+	}
+}
---
+++ Assets/Target.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Target : MonoBehaviour {
+
+    public int agent;
+    public int mDist;
+    public GameObject prefab;
+
+    void Awake()
+    {
+        for(int i = 0; i < agent; i++)
+        {
+            Vector3 pos = new Vector3();
+            pos.x = Random.Range(-mDist, mDist);
+            pos.y = Random.Range(-mDist, mDist);
+            pos.z = Random.Range(-mDist, mDist);
+
+            GameObject temp = Instantiate(prefab,pos, Quaternion.identity) as GameObject;
+            Agent a = temp.GetComponent<Agent>();
+            a.target = gameObject.transform;
+            a.mass = Random.Range(5, 20);
+        }
+    }
+}
